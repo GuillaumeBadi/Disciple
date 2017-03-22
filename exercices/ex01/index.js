9b2ac5b34225a017e38b28a10f5d2f5b8aa46473
Emmanuelle TERMEAU <emtermea@e2r6p11.42.fr>
2017-03-22 20:08:07 +0100

    success

--- exercices/ex01/index.js
+++ exercices/ex01/index.js
@@ -4,3 +4,13 @@
     wich takes two strings and add them together,
     and a getLength function, wich takes a string and return its length
 */
+
+function getLength (str) {
+  return str.length
+}
+
+function concatenate (str1, str2) {
+  return str1.concat(str2)
+}
+
+export { concatenate, getLength }
