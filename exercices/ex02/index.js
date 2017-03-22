e08859a31c54d775280e3fed8af02a48937072a2
Emmanuelle TERMEAU <emtermea@e2r6p11.42.fr>
2017-03-22 20:55:20 +0100

    success

--- exercices/ex02/index.js
+++ exercices/ex02/index.js
@@ -3,3 +3,12 @@
  * export a function that takes a number (an age) and return true
  *  if this is old enough to drink, false otherwise
  */
+
+function check (age) {
+  if (age >= 18) {
+    return true
+  }
+  return false
+}
+
+export default check
